a95d540c09153b82eae45220fccfdb18fa3d761f
hbsambo1231 <hsambo89410@uat.edu>
2025-09-12 23:53:57 -0500

    Add files via upload
    first Assignment 2.1

---
+++ 5 COMPONENTS/# pythonsample.py
@@ -0,0 +1,11 @@
+# pythonsample.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
+    print("This is a new beginner pythone file.")
+    print(I like to code and learn new things.)
+    print("wow amazing work done by me.")
